810ae6315646bef9f95529d851b8689228efd534
Matthew Chieco <cheeks369@gmail.com>
2021-08-15 21:24:25 -0400

    fix issues

--- keyboards/primekb/meridian/keymaps/mchieco/keymap.c
+++ keyboards/primekb/meridian/keymaps/mchieco/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
 		LT(3, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
 		KC_LSPO, KC_Z, KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                   KC_DEL,
-		LCTL_T(KC_LBRC), LGUI_T(KC_LCBR), LALT_T(KC_LABK), LT(1, KC_SPC),   LT(2, KC_SPC),  RALT_T(KC_RABK), RGUI_T(KC_RCBR), RCTL_T(KC_RBRC), _______
+		LCTL_T(KC_LBRC), LGUI_T(KC_LCBR), LALT_T(KC_LEFT_ANGLE_BRACKET), LT(1, KC_SPC),   LT(2, KC_SPC),  RALT_T(KC_RIGHT_ANGLE_BRACKET), RGUI_T(KC_RCBR), RCTL_T(KC_RBRC), _______
     ),
     [1] = LAYOUT_split_bs_rshift(
         RESET,   _______, _______, _______,  _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
